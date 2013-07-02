04ed32b091c864adbc5498d47f9426b74ad661c6
agordo <albert.gordo.s@gmail.com>
2013-07-02 19:27:09 +0200

    Major changes in solver without pq

--- C/core.h
+++ C/core.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,35 +6,26 @@
 #include <assert.h>
 #include <math.h>
 #include <sys/time.h>
-
+#pragma once
+#include "core.h"
 #include "aux.h"
 
 
+void train_epoch(int Ntrain, int d, float *Xtrain, int *Ltrain,  int* perm,
+        sgd_params_t *params, int updateEta, int *t,  float *W, float *B);
 
 
 
 
-
-void train_epoch(int Ntrain, int d, float * Xtrain, int *Ltrain,  int* perm,
-        sgd_params_t *params, int *t,  float *W, float *B);
-
-void train_epoch_fixed_eta(int Ntrain, int d, float * Xtrain, int *Ltrain,  int* perm,
-        float eta,  sgd_params_t *params, float *W, float *B);
-
-
-float evaluateEta(int Ntrain, int d,  int nmax, float *Xtrain, int* Ltrain, int *perm, sgd_params_t *params, float eta);
-float determineEta0(int Ntrain, int d,  int nmax,float *Xtrain,  int* Ltrain,  sgd_params_t *params);
-
-
-void sgd_train_class_cv(int cls,
+void sgd_train_class_cv(int cls,	
         int Ntrain,int d,          
         float *Xtrain,
-        int *_Ltrain,
-	    int Nval,
+        int *Ltrain,
+	int Nval,
         float * Xval,
         int *_Lval,
         sgd_cv_params_t *params_cv,
-    	float *W, float *B,
+	float *W, float *B,
         float *PlattsA, float *PlattsB,
         sgd_output_info_t *output);
 
@@ -43,10 +33,15 @@ void sgd_train_class(int cls,
         int Ntrain,int d,          
         float *Xtrain,
         int *Ltrain,
-    	int Nval,
-        float * Xval,
+	int Nval,
+        float *Xval,
         int *Lval,
         sgd_params_t *params,
-    	float *W, float *B,
+	float *W, float *B,
         sgd_output_info_t *output);
+
+
+
+
+
 
--- python/sgd_wrap.py
+++ python/sgd_wrap.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import ctypeslib
 import ctypes
 from ctypes import *
-lsgd = ctypes.cdll.LoadLibrary("/home/lear/gordo/Code/sgd_albert2/libsgd2.so")
+lsgd = ctypes.cdll.LoadLibrary("../libsgdsvm.so")
 
 lsgd.Platts.argtypes = [np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*scores
                         ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Lval  
@@ -111,31 +111,17 @@ lsgd.sgd_train_class_pq.argtypes = [c_int # c
 
 
 
-
-
-def sgdalbert_train_cv_pq(pq,Xtrain_pqcodes,Ltrain,Xval_pqcodes,Lval, params):
-    nc = np.max(Ltrain)+1
-    infos = [None]*nc
-    d = pq.nsq*pq.dsq
+def sgdalbert_train_cv_oneclass(cls,  Xtrain,Ltrain,Xval,Lval,  params):
+    d = Xtrain.shape[1]
     ntrain = len(Ltrain)
     nval = len(Lval)
-    W = np.zeros((nc,d),dtype = np.float32)
-    Bias = [None]*nc
-    PlattsA = [None]*nc
-    PlattsB = [None]*nc
-    for c in range(nc):
-        print "Doing class %d/%d"%(c,nc)
-        info = sgd_output_info_t()
-        bias_tmp=c_float()
-        plattsA_tmp=c_float()
-        plattsB_tmp=c_float()    
-        lsgd.sgd_train_class_cv_pq(c,byref(pq),  ntrain,d,  Xtrain_pqcodes,Ltrain.T[0],nval, Xval_pqcodes,Lval.T[0],byref(params),W[c], byref(bias_tmp), byref(plattsA_tmp), byref(plattsB_tmp), byref(info))
-        infos[c] = info
-        Bias[c] = bias_tmp.value
-        PlattsA[c] = plattsA_tmp.value
-        PlattsB[c] = plattsB_tmp.value    
-    return W,np.array(Bias, dtype=np.float32),np.array(PlattsA, dtype=np.float32),np.array(PlattsB, dtype=np.float32),infos
-
+    W = np.zeros(d,dtype = np.float32)
+    bias_tmp=c_float()
+    plattsA_tmp=c_float()
+    plattsB_tmp=c_float()    
+    info = sgd_output_info_t()
+    lsgd.sgd_train_class_cv(ntrain,d,  Xtrain,Ltrain.T[0],nval, Xval,Lval.T[0],byref(params),W, byref(bias_tmp), byref(plattsA_tmp), byref(plattsB_tmp), byref(info))
+    return W,bias_tmp.value,plattsA_tmp.value,plattsB_tmp.value,info
 
 
 def sgdalbert_train_cv_pq_oneclass(cls,pq,  Xtrain_pqcodes,Ltrain,Xval_pqcodes,Lval,  params):
@@ -150,139 +136,3 @@ def sgdalbert_train_cv_pq_oneclass(cls,pq,  Xtrain_pqcodes,Ltrain,Xval_pqcodes,L
     lsgd.sgd_train_class_cv_pq(cls,byref(pq),  ntrain,d,  Xtrain_pqcodes,Ltrain.T[0],nval, Xval_pqcodes,Lval.T[0],byref(params),W, byref(bias_tmp), byref(plattsA_tmp), byref(plattsB_tmp), byref(info))
     return W,bias_tmp.value,plattsA_tmp.value,plattsB_tmp.value,info
 
-def sgdalbert_update_pq_oneclass(cls, pq, W,Bias, Xtrain_pqcodes,Ltrain_, params):
-    Ltrain = Ltrain_.copy()
-    d = pq.nsq*pq.dsq
-    ntrain = len(Ltrain)
-    info = sgd_output_info_t()
-    ppos = np.where(Ltrain==cls)
-    pneg = np.where(Ltrain!=cls)    
-    Ltrain[ppos] = 1
-    Ltrain[pneg] = -1
-    bias_tmp=c_float(Bias/params.bias_multiplier)
-    lsgd.sgd_train_class_pq(cls,byref(pq),ntrain, d, Xtrain_pqcodes,Ltrain.T[0],0, np.empty([1], dtype = c_uint8), np.empty([1], dtype = c_int32),byref(params),W, byref(bias_tmp), byref(info))
-    return W,bias_tmp.value*params.bias_multiplier,info.t
-
-
-
-'''
-lsgd.sgd_train_class_pq.argtypes = [c_int # c
-                , POINTER(pq_info_t)
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*W
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*B
-                           ,c_int #d
-                           ,c_int #Ntrain
-                           ,c_int #Nval
-                           ,np.ctypeslib.ndpointer(dtype=c_uint8, ndim=1) #Xtrain_pq
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Ltrain
-                           ,np.ctypeslib.ndpointer(dtype=c_uint8, ndim=1) #Xval_pq
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Lval  
-                           ,POINTER(sgd_params_t)
-                           ,POINTER(sgd_output_info_t)]
-
-lsgd.train_epoch_pq.argtypes = [POINTER(pq_info_t)
-                           ,c_int #Ntrain
-                           ,c_int #d
-                           ,np.ctypeslib.ndpointer(dtype=c_uint8, ndim=1) #Xtrain_pq
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Ltrain
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #perm                          
-                           ,POINTER(sgd_params_t) # params
-                           ,POINTER(c_int32) # t
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*W
-                           ,POINTER(c_float)] #*B
-
-
-
-
-def sgdalbert_update_pq_oneclass(cls, pq, W,Bias, Xtrain_pqcodes,Ltrain_, params):
-    Ltrain = Ltrain_.copy()
-    d = pq.nsq*pq.dsq
-    ntrain = len(Ltrain)
-    info = sgd_output_info_t()
-    ppos = np.where(Ltrain==cls)
-    pneg = np.where(Ltrain!=cls)    
-    Ltrain[ppos] = 1
-    Ltrain[pneg] = 1
-    import pdb;pdb.set_trace()
-    lsgd.sgd_train_class_pq(cls,byref(pq),W[0], Bias[0:1], d, ntrain,0, Xtrain_pqcodes,Ltrain.T[0],np.empty([1], dtype = c_uint8), np.empty([1], dtype = c_int32),byref(params), byref(info))
-    return W,Bias,info
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-lsgd.sgd_train_class_cv.argtypes = [c_int # c
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*W
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*B
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*PlattsA
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=1) #*PlattsB
-                           ,c_int #d
-                           ,c_int #Ntrain
-                           ,c_int #Nval
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=2) #Xtrain
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Ltrain
-                           ,np.ctypeslib.ndpointer(dtype=c_float, ndim=2) #Xval
-                           ,np.ctypeslib.ndpointer(dtype= c_int, ndim=1) #Lval  
-                           ,POINTER(sgd_cv_params_t)
-                           ,POINTER(sgd_output_info_t)]
-
-def sgdalbert_train_cv(Xtrain,Ltrain,Xval,Lval,params):
-    nc = np.max(Ltrain)+1
-    infos = [None]*nc
-    ntrain, d = Xtrain.shape
-    nval = Xval.shape[0]
-    W = np.zeros((nc,d),dtype = np.float32)
-    Bias = np.zeros([nc],dtype = np.float32)
-    PlattsA = np.zeros([nc],dtype = np.float32)
-    PlattsB = np.zeros([nc],dtype = np.float32)
-    for c in range(nc):
-        print "Doing class %d/%d"%(c,nc)
-        info = sgd_output_info_t()
-        lsgd.sgd_train_class_cv(c,W[c], Bias[c:(c+1)], PlattsA[c:(c+1)], PlattsB[c:(c+1)], d, ntrain,nval, Xtrain,Ltrain.T[0],Xval,Lval.T[0],byref(params), byref(info))
-        infos[c] = info
-    return W,Bias,PlattsA,PlattsB,infos
-'''
-
-
-'''
-def compute_map(s,l):
-    ranks=[]
-    nrl = 0
-    acc = 0
-    for i in range(len(l)):
-        if l[i]==-1: continue
-        c=0
-        c_eq=0
-        for j in range(len(l)):
-            if i ==j: continue
-            if s[j] > s[i]: c+=1
-            elif s[j]==s[i]: c_eq+=1
-        c += c_eq/2
-        ranks.append(c)
-        nrl+=1
-    ranks.sort()
-    for i in range(len(ranks)):
-        acc += (i+1.0)/(ranks[i]+1.0)
-    acc/=nrl        
-    return acc
-    '''
